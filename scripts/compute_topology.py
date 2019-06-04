dab9edc7e21d0ad6665b65b35c6fe3b6f6cbbc08
CiprianCorneanu <CiprianCorneanu>
2019-06-04 16:00:08 +0200

    Changed dipha build path.

--- scripts/compute_topology.py
+++ scripts/compute_topology.py
@@ -19,5 +19,5 @@ for e in args.epochs:
         for t in args.thresholds:
             os.system("../cpp/symmetric_b1 "+path+"badj_epc"+str(e)+'_t{:1.4f}'.format(t)+"_trl"+args.trial+".csv 1 0")
     elif args.homology_type == 'persistent':
-        os.system("../dipha/full_to_sparse_distance_matrix "+str(T)+" "+path+"adj_epc{}_trl{}.bin ".format(e, args.trial)+path+"adj_epc{}_trl{}_{}.bin".format(e, args.trial, T))
-        os.system("mpiexec -n "+str(NPROC)+" ../dipha/dipha --upper_dim "+str(UPPER_DIM)+" --benchmark  --dual "+path+"adj_epc{}_trl{}_{}.bin ".format(e, args.trial, T)+path+"adj_epc{}_trl{}_{}.bin.out".format( e, args.trial, T))
+        os.system("../dipha/build/full_to_sparse_distance_matrix "+str(T)+" "+path+"adj_epc{}_trl{}.bin ".format(e, args.trial)+path+"adj_epc{}_trl{}_{}.bin".format(e, args.trial, T))
+        os.system("mpiexec -n "+str(NPROC)+" ../dipha/build/dipha --upper_dim "+str(UPPER_DIM)+" --benchmark  --dual "+path+"adj_epc{}_trl{}_{}.bin ".format(e, args.trial, T)+path+"adj_epc{}_trl{}_{}.bin.out".format( e, args.trial, T))
