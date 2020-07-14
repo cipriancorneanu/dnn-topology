8e807512573ccfbd8def32c699120e119dfbfe42
Ciprian Corneanu <cipriancorneanu@Ciprians-MacBook-Pro.local>
2020-07-14 09:58:18 +0200

    Add test.

--- models/densenet.py
+++ models/densenet.py
@@ -10,14 +10,14 @@ class Bottleneck(nn.Module):
     def __init__(self, in_planes, growth_rate):
         super(Bottleneck, self).__init__()
         self.bn1 = nn.BatchNorm2d(in_planes)
-        self.conv1 = nn.Conv2d(in_planes, 4*growth_rate, kernel_size=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(4*growth_rate)
-        self.conv2 = nn.Conv2d(4*growth_rate, growth_rate, kernel_size=3, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(in_planes, 4 * growth_rate, kernel_size=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(4 * growth_rate)
+        self.conv2 = nn.Conv2d(4 * growth_rate, growth_rate, kernel_size=3, padding=1, bias=False)
 
     def forward(self, x):
         out = self.conv1(F.relu(self.bn1(x)))
         out = self.conv2(F.relu(self.bn2(out)))
-        out = torch.cat([out,x], 1)
+        out = torch.cat([out, x], 1)
         return out
 
 
@@ -38,29 +38,29 @@ class DenseNet(nn.Module):
         super(DenseNet, self).__init__()
         self.growth_rate = growth_rate
 
-        num_planes = 2*growth_rate
+        num_planes = 2 * growth_rate
         self.conv1 = nn.Conv2d(3, num_planes, kernel_size=3, padding=1, bias=False)
 
         self.dense1 = self._make_dense_layers(block, num_planes, nblocks[0])
-        num_planes += nblocks[0]*growth_rate
-        out_planes = int(math.floor(num_planes*reduction))
+        num_planes += nblocks[0] * growth_rate
+        out_planes = int(math.floor(num_planes * reduction))
         self.trans1 = Transition(num_planes, out_planes)
         num_planes = out_planes
 
         self.dense2 = self._make_dense_layers(block, num_planes, nblocks[1])
-        num_planes += nblocks[1]*growth_rate
-        out_planes = int(math.floor(num_planes*reduction))
+        num_planes += nblocks[1] * growth_rate
+        out_planes = int(math.floor(num_planes * reduction))
         self.trans2 = Transition(num_planes, out_planes)
         num_planes = out_planes
 
         self.dense3 = self._make_dense_layers(block, num_planes, nblocks[2])
-        num_planes += nblocks[2]*growth_rate
-        out_planes = int(math.floor(num_planes*reduction))
+        num_planes += nblocks[2] * growth_rate
+        out_planes = int(math.floor(num_planes * reduction))
         self.trans3 = Transition(num_planes, out_planes)
         num_planes = out_planes
 
         self.dense4 = self._make_dense_layers(block, num_planes, nblocks[3])
-        num_planes += nblocks[3]*growth_rate
+        num_planes += nblocks[3] * growth_rate
 
         self.bn = nn.BatchNorm2d(num_planes)
         self.linear = nn.Linear(num_planes, num_classes)
@@ -85,26 +85,34 @@ class DenseNet(nn.Module):
 
     def forward_features(self, x):
         return []
-    
+
+
 def DenseNet121(num_classes):
-    return DenseNet(Bottleneck, [6,12,24,16], growth_rate=32, num_classes=num_classes)
+    return DenseNet(Bottleneck, [6, 12, 24, 16], growth_rate=32, num_classes=num_classes)
+
 
 def DenseNet169():
-    return DenseNet(Bottleneck, [6,12,32,32], growth_rate=32, num_classes=num_classes)
+    return DenseNet(Bottleneck, [6, 12, 32, 32], growth_rate=32, num_classes=num_classes)
+
 
 def DenseNet201():
-    return DenseNet(Bottleneck, [6,12,48,32], growth_rate=32, num_classes=num_classes)
+    return DenseNet(Bottleneck, [6, 12, 48, 32], growth_rate=32, num_classes=num_classes)
+
 
 def DenseNet161():
-    return DenseNet(Bottleneck, [6,12,36,24], growth_rate=48, num_classes=num_classes)
+    return DenseNet(Bottleneck, [6, 12, 36, 24], growth_rate=48, num_classes=num_classes)
+
 
 def densenet_cifar():
-    return DenseNet(Bottleneck, [6,12,24,16], growth_rate=12, num_classes=num_classes)
+    return DenseNet(Bottleneck, [6, 12, 24, 16], growth_rate=12, num_classes=num_classes)
+
 
 def test():
     net = densenet_cifar()
-    x = torch.randn(1,3,32,32)
+    x = torch.randn(1, 3, 32, 32)
     y = net(x)
     print(y)
 
-# test()
+
+if __name__ == '__main__':
+    test()
