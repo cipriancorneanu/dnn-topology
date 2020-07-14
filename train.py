900808e2b87db81f9e611bf36d299e2828b8c211
Ciprian Corneanu <cipriancorneanu@Ciprians-MacBook-Pro.local>
2020-07-14 10:00:09 +0200

    Clean imports, correct saving of epoch 0 checkpoint.

--- train.py
+++ train.py
@@ -1,21 +1,14 @@
 from __future__ import print_function
 import argparse
-import pickle as pkl
-import numpy as np
-import h5py
-from utils import *
 from passers import Passer
-from savers import save_activations, save_checkpoint, save_losses
+from savers import save_checkpoint, save_losses
 from loaders import *
 from labels import *
 from models.utils import get_model, get_criterion
 import torch
-import torch.nn as nn
 import torch.optim as optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
-import torchvision
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--net')
@@ -81,7 +74,7 @@ manipulator = load_manipulator(args.permute_labels, args.binarize_labels)
 
 ''' Make intial pass before any training '''
 loss_te, acc_te = passer_test.run()
-save_checkpoint(checkpoint = {'net':net.state_dict(), 'acc': acc_te, 'epoch': 0}, path='./checkpoint/'+ONAME, fname='ckpt_trial_'+str(args.trial)+'_epoch_0.t7')
+save_checkpoint(checkpoint = {'net':net.state_dict(), 'acc': acc_te, 'epoch': 0}, path='./checkpoint/'+ONAME+'/', fname='ckpt_trial_'+str(args.trial)+'_epoch_0.t7')
 
 losses = []
 for epoch in range(start_epoch, start_epoch+args.epochs):
@@ -97,4 +90,4 @@ for epoch in range(start_epoch, start_epoch+args.epochs):
         save_checkpoint(checkpoint = {'net':net.state_dict(), 'acc': acc_te, 'epoch': epoch}, path='./checkpoint/'+ONAME+'/', fname='ckpt_trial_'+str(args.trial)+'_epoch_'+str(epoch)+'.t7')
 
 '''Save losses'''
-save_losses(losses, path='./losses/'+ONAME+'/', fname='stats_trial_' +str(args.trial) +'.pkl')
+save_losses(losses, path='./losses/'+ONAME+'/', fname='stats_trial_' + str(args.trial) +'.pkl')
