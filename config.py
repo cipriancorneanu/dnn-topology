42eb1b9885138d113dd6a2dea13cb6efb8e9db6b
Ciprian Corneanu <cipriancorneanu@Ciprians-MacBook-Pro.local>
2020-07-14 09:52:36 +0200

    Improve comments and update parameters.

--- config.py
+++ config.py
@@ -1,19 +1,15 @@
-import numpy as np
-
-# For what threholds I am going to create binary graphs for computing topology
-THRESHOLDS = np.arange(0.50, 1, 0.05)
-
 # Where to save topology results
-SAVE_PATH='./results_dnn_topology'
+SAVE_PATH = 'results_dnn_topology'
 
-# DIPHA
+#  DIPHA
 DIPHA_MAGIC_NUMBER = 8067171840
 ID = 7
-T = 0.4
+
+#  Sets upper limit for epsilon range in sparse distance matrix computation
+MAX_EPSILON = 0.3
 
 # Number of cores MPI can use
 NPROC = 4
 
-# Sets dimensions to compute persistent homology. If '2' only the first betti curve will be computed.
-# For first and second betti curves set to '3' etc.
-UPPER_DIM = 2
+# Sets upper limit to dimension to compute for persistent homology.
+UPPER_DIM = 3
