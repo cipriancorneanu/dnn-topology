ddad80fd305c63ff9301e6fefc8cce6f9d006c04
Ciprian Corneanu <cipriancorneanu@Ciprians-MacBook-Pro.local>
2020-07-14 09:56:03 +0200

    Remove static homology computations.

--- build_graph_functional.py
+++ build_graph_functional.py
@@ -1,21 +1,8 @@
 from __future__ import print_function
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.optim.lr_scheduler import ReduceLROnPlateau
-import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
-import torchvision
-import os
-import argparse
-from utils import progress_bar
-import numpy as np
-import h5py
-import pickle 
 from utils import *
-from models.utils import get_model, get_criterion
+from models.utils import get_model
 from passers import Passer
-from savers import save_activations, save_checkpoint, save_losses
 from loaders import *
 from graph import *
 from labels import load_manipulator
@@ -29,7 +16,6 @@ parser.add_argument('--epochs', nargs='+', type=int)
 parser.add_argument('--split', default=0, type=int)
 parser.add_argument('--kl', default=0, type=int)
 parser.add_argument('--input_size', default=32, type=int)
-parser.add_argument('--thresholds', nargs='+', type=float)
 parser.add_argument('--filtration', default='nominal')
 parser.add_argument('--permute_labels', default=0, type=float)
 parser.add_argument('--binarize_labels', default=-1, type=int)
@@ -38,10 +24,8 @@ args = parser.parse_args()
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
     
 ''' Meta-name to be used as prefix on all savings'''
-oname = args.net + '_' + args.dataset + '/'
-SAVE_DIR = args.save_path + 'adjacency/' + oname
+SAVE_DIR = os.path.join(args.save_path, args.net + '_' + args.dataset + '/')
 START_LAYER = 3 if args.net in ['vgg', 'resnet'] else 0 
-THRESHOLDS = args.thresholds
 
 ''' If save directory doesn't exist create '''
 if not os.path.exists(SAVE_DIR):
@@ -55,6 +39,8 @@ net = net.to(device)
 if device == 'cuda':
     net = torch.nn.DataParallel(net)
     cudnn.benchmark = True
+
+print(net)
 
 ''' Prepare criterion '''
 criterion = nn.CrossEntropyLoss()
@@ -81,18 +67,3 @@ for epoch in args.epochs:
 
     ''' Write adjacency to binary. To use as DIPHA input for persistence homology '''
     save_dipha(SAVE_DIR + 'adj_epc{}_trl{}.bin'.format(epoch, args.trial), 1-adj)
-
-    ''' Compute thresholds. If nominal, use args.thresholds directly, if density, compute nominal correspoding to edge densitites first. For static homology. '''
-    if args.filtration == 'density':
-        edge_t = [build_density_adjacency(adj, t) for t in args.thresholds]
-        print('The edge thresholds correspoding to required densities are: {}'.format(edge_t))
-    
-        for et, dt in zip(edge_t, args.thresholds):
-            badj = binarize(np.copy(adj), et)
-            print('Taking T={}, density={}'.format(et, np.sum(badj)/np.prod(adj.shape)))
-            np.savetxt(SAVE_DIR + 'badj_epc{}_t{:1.4f}_trl{}.csv'.format(epoch, dt, args.trial), badj, fmt='%d', delimiter=",")
-    elif args.filtration == 'nominal':
-        print('Size of adjacency matrix is {}'.format(adj.shape))
-        for threshold in args.thresholds:
-            badj = binarize(np.copy(adj), threshold)
-            np.savetxt(SAVE_DIR + 'badj_epc{}_t{:1.4f}_trl{}.csv'.format(epoch, threshold, args.trial), badj, fmt='%d', delimiter=",")
